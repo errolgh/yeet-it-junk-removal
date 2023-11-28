13803268e1b532647f59bd10ba1acd7c2a263cf0
Errol Watson <ewatsonrei@gmail.com>
2023-11-28 14:26:57 -0500

    updates nav items alignment

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -31,17 +31,17 @@ export default function Nav() {
         absolute z-30 top-0 left-full w-3/5
         pr-12 pt-24 text-right h-screen bg-black lg:bg-transparent
         flex flex-col space-y-6 font-alata md:p-4 md:h-10 md:bg-transparent md:w-min
-        md:visible md:flex-row md:space-x-8 md:space-y-0 md:static md:mb-6`}
+        md:visible md:flex-row md:space-x-8 md:space-y-0 md:static md:mb-6 md:pt-10 md:items-center`}
         >
           <li className="hover:text-teal-400">
             <Link to="/" activeClassName="active">
               Home
             </Link>
           </li>
-          <li className='flex place-content-end -mr-3'>
+          <li className='flex place-content-end -mr-3 bg-black rounded-xl h-10 pr-3'>
             <a
               href="tel:4438390909"
-              className="bg-black rounded-xl h-10 flex relative w-44 pt-1"
+              className="flex relative w-44 pt-1"
             >
               <div className='text-right'>
                 <FaPhone className="absolute text-yellow-600 top-3 left-3" />
