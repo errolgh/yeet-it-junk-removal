ee60ce1b02aec0f9b7065cd3ef81d7f5c89ad0c4
Errol Watson <ewatsonrei@gmail.com>
2023-11-28 13:31:53 -0500

    update 404, removing warnings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ import Facebook from '../images/svg/icon-facebook.svg'
 import Twitter from '../images/svg/icon-twitter.svg'
 import Pinterest from '../images/svg/icon-pinterest.svg'
 import Instagram from '../images/svg/icon-instagram.svg'
-import { AnchorLink } from 'gatsby-plugin-anchor-links'
 import { Link } from 'gatsby'
 
 export default function Footer() {
