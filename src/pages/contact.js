4f698b742606fc5914429395ee06348b6f4af630
Errol Watson <ewatsonrei@gmail.com>
2024-07-17 15:58:34 -0400

    social links and button colors

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,18 @@
-import * as React from 'react'
-import { IoLogoFacebook } from 'react-icons/io5'
+import * as React from 'react';
+import { IoLogoFacebook } from 'react-icons/io5';
 // import { IoLogoTwitter } from 'react-icons/io5'
 // import { IoLogoLinkedin } from 'react-icons/io5'
-import { IoLogoInstagram } from 'react-icons/io5'
-import { IoCall } from 'react-icons/io5'
-import { IoMailSharp } from 'react-icons/io5'
-import { IoLocationSharp } from 'react-icons/io5'
-import { Link } from 'gatsby'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-import CanBg from '../images/svg/canBg.svg'
-import Layout from '../components/Layout'
+import { IoLogoInstagram } from 'react-icons/io5';
+import { IoCall } from 'react-icons/io5';
+import { IoMailSharp } from 'react-icons/io5';
+import { IoLocationSharp } from 'react-icons/io5';
+import { Link } from 'gatsby';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import CanBg from '../images/svg/canBg.svg';
+import Layout from '../components/Layout';
+import Button from '../components/Button'
 
 // @TODO: once netlify is connected, check gatsby/netlify docs for anything else
 
@@ -19,7 +20,7 @@ import Layout from '../components/Layout'
 
 // const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i
 const phoneRegExp =
-  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 const schema = yup.object().shape({
   name: yup.string().required('Your Name is required'),
@@ -27,7 +28,7 @@ const schema = yup.object().shape({
   email: yup.string().email().required('Email Address is required'),
   number: yup.string().matches(phoneRegExp, 'Phone number is not valid'),
   message: yup.string(),
-})
+});
 
 const ContactForm = () => {
   const {
@@ -36,44 +37,52 @@ const ContactForm = () => {
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
-  })
+  });
 
   // handleSubmit & netlify-forms both provide a "data" object
   const onSubmit = (data) => {
-    console.log('first', data)
-  }
+    console.log('first', data);
+  };
   return (
     <Layout>
-      <section id="contact" className="flex mb-20 md:mt-6 min-h-full md:mb-24 justify-center items-center">
+      <section
+        id="contact"
+        className="flex mb-20 md:mt-6 min-h-full md:mb-24 justify-center items-center"
+      >
         <div className="flex flex-col space-y-6 w-full max-w-5xl p-8 md:flex-row md:space-x-6 md:space-y-0">
           <div className="relative flex flex-col justify-between space-y-8 md:p-12 lg:p-6">
             <div>
               <h1 id="contact"> Contact Us</h1>
               <p className="pt-4">
-                Filling out this form will notify someone from our team. Let
-                us know what your needs are so we can provide what we do best!
+                Filling out this form will notify someone from our team. Let us
+                know what your needs are so we can provide what we do best!
               </p>
             </div>
             <div className="flex flex-col space-y-6">
-                <a href="sms:+14438390909">
-              <div className="inline-flex space-x-2 items-center">
-                  <IoCall className="text-yellow-600 text-xl" />
+              <a href="sms:+19084721977">
+                <div className="inline-flex space-x-2 items-center">
+                  <IoCall className="text-junkGreen text-xl" />
                   <span className="text-gray-600">(908) 472-1977</span>
-              </div>
-                </a>
-                <a href="mailto:LetsTalkNevermore@gmail.com">
+                </div>
+              </a>
+              <a href="mailto:LetsTalkNevermore@gmail.com">
+                <div className="inline-flex space-x-2 items-center">
+                  <IoMailSharp className="text-junkGreen text-xl" />
+                  <span className="text-gray-600">
+                    LetsTalkNevermore@gmail.com
+                  </span>
+                </div>
+              </a>
               <div className="inline-flex space-x-2 items-center">
-                  <IoMailSharp className="text-yellow-600 text-xl" />
-                  <span className="text-gray-600">LetsTalkNevermore@gmail.com</span>
-              </div>
-                </a>
-              <div className="inline-flex space-x-2 items-center">
-                <IoLocationSharp className="text-yellow-600 text-xl" />
+                <IoLocationSharp className="text-junkGreen text-xl" />
                 <span className="text-gray-600">Baltimore, MD</span>
               </div>
             </div>
-            <div className="flex text-lg space-x-4 text-yellow-600">
-              <Link to="https://www.facebook.com/profile.php?id=61553945744042">
+            <div className="flex text-lg space-x-4 text-junkGreen">
+              <Link
+                target="_blank"
+                to="https://www.facebook.com/people/Nevermore-Cleaning-Junk-Removal/61560563170433/"
+              >
                 <IoLogoFacebook />
               </Link>
               {/* <Link to="/">
@@ -82,7 +91,10 @@ const ContactForm = () => {
             <Link to="/">
               <IoLogoLinkedin />
             </Link> */}
-              <Link to="https://www.instagram.com/yeet_itjunkremoval">
+              <Link
+                target="_blank"
+                to="https://www.instagram.com/letstalknevermore"
+              >
                 <IoLogoInstagram />
               </Link>
             </div>
@@ -167,12 +179,13 @@ const ContactForm = () => {
                 {/* photos for submission */}
 
                 {/* Submission Elements */}
-                <button
+                <Button
+                  order="primary"
                   type="submit"
                   className="inline-block w-full md:w-auto self-end bg-brandPrimary font-bold text-white rounded-lg px-6 py-2 uppercase"
                 >
                   Send
-                </button>
+                </Button>
                 {/* as per netlify forms: */}
                 <input
                   type="hidden"
@@ -186,7 +199,7 @@ const ContactForm = () => {
         </div>
       </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
